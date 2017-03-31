9ec5439d04862beefc427f967d3e5125febcd69e
Nicholas G Reich <nick@schoolph.umass.edu>
2017-03-31 16:24:31 -0400

    updated name disentanglement. First viable install of the package.

---
+++ R/densitystackr-package.R
@@ -0,0 +1,11 @@
+#'densitystackr - Stacking via xgboost
+#'
+#'An implementation of model stacking using xgboost.
+#'
+#'@author Evan Ray <elray@umass.edu>
+#'@docType package
+#'@name densitystackr
+#'@importFrom Rcpp evalCpp
+#'@useDynLib densitystackr, .registration = TRUE
+#'@aliases densitystackr densitystackr-package
+NULL
